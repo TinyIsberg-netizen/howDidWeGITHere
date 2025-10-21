5a47be09d4fc262c27effbbdc6d0cbd916c0bfda
Filip <isberget23@gmail.com>
2025-10-21 14:42:20 +0200

    modified:   Character.java

--- Character.java
+++ Character.java
@@ -1,11 +1,11 @@
 abstract class Character {
-public static void main (String [] args){
+
 //Strength, Dexterity, Constitution
     int dexterity;
     int strength;
     int constitution;
     
-    public Character(int dexterity, int strength, int constitution) {
+public Character(int dexterity, int strength, int constitution) {
         this.dexterity = dexterity;
         this.strength = strength;
         this.constitution = constitution;
@@ -13,4 +13,4 @@ public static void main (String [] args){
     }
     
 }
-}
+
