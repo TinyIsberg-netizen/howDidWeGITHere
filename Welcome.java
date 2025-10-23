eed87d20d424b3beebaa8806e03e1b103737634a
Filip <isberget23@gmail.com>
2025-10-23 13:55:04 +0200

    Created loop for wrong answer Race

--- Welcome.java
+++ Welcome.java
@@ -7,27 +7,30 @@ public class Welcome {
     Orc myOrc = new Orc();
     Human myHuman = new Human();
     Elve myElve = new Elve();
-
+    
     public static void main(String[] args) {
+
     Scanner scan = new Scanner(System.in);    
     System.out.println("Welcome brave hero to The Tower of Tom!\nPlease enter your characters name: ");
 
-
+    boolean validRace = false;
     String characterName = scan.nextLine();
-     System.out.println("Welcome " + characterName + "\nWhat's your race?" + "\n1.Human - Average \n2.Elve - Fast \n3.Orc - Strong" );
+    while (!validRace) {
+        
+    System.out.println("Welcome " + characterName + "\nWhat's your race?" + "\n1.Human - Average \n2.Elve - Fast \n3.Orc - Strong" );
         String race = scan.nextLine();
      
         if (race.equals("Orc")){
             System.out.println("You chose Orc" + "\nWow! Orc, you dont say!" + " Haven't seen alot of your kind here!\nAnyway, good luck not getting squished.");
-
+        validRace = true;
             
         }else if(race.equals("Human")){
             System.out.println("You chose Human" + "\nWow! Human, you dont say!" + " Haven't seen alot of your kind here!\nAnyway, good luck not getting squished.");
-
+        validRace = true;
         }else if(race.equals("Elve")){
             System.out.println("You chose Elve" + "\nWow! Elve, you dont say!" + " Haven't seen alot of your kind here!\nAnyway, good luck not getting squished.");
-            
-        }else{System.out.println("Thats not a race, you silly goose!");
+        validRace = true;   
+        }else{System.out.println("Thats not a race, you silly goose! Try Again.");
             
         }
 
@@ -37,6 +40,6 @@ public class Welcome {
         
     }
     
-
+}
     
 
