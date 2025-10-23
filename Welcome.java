b7e4dac5d8b39a212a29e5a621855a060f564574
Filip <isberget23@gmail.com>
2025-10-23 14:32:05 +0200

    Ignores upper or lower cases on choosing race.

--- Welcome.java
+++ Welcome.java
@@ -12,24 +12,27 @@ public class Welcome {
 
     Scanner scan = new Scanner(System.in);    
     System.out.println("Welcome brave hero to The Tower of Tom!\nPlease enter your characters name: ");
-
+        
     boolean validRace = false;
     String characterName = scan.nextLine();
+    
     while (!validRace) {
         
     System.out.println("Welcome " + characterName + "\nWhat's your race?" + "\n1.Human - Average \n2.Elve - Fast \n3.Orc - Strong" );
         String race = scan.nextLine();
      
-        if (race.equals("Orc")){
+        if (race.equalsIgnoreCase("orc")){
             System.out.println("You chose Orc" + "\nWow! Orc, you dont say!" + " Haven't seen alot of your kind here!\nAnyway, good luck not getting squished.");
-        validRace = true;
+            validRace = true;
             
-        }else if(race.equals("Human")){
+        }else if(race.equalsIgnoreCase("human")){
             System.out.println("You chose Human" + "\nWow! Human, you dont say!" + " Haven't seen alot of your kind here!\nAnyway, good luck not getting squished.");
-        validRace = true;
-        }else if(race.equals("Elve")){
+            validRace = true;
+
+        }else if(race.equalsIgnoreCase("elve")){
             System.out.println("You chose Elve" + "\nWow! Elve, you dont say!" + " Haven't seen alot of your kind here!\nAnyway, good luck not getting squished.");
-        validRace = true;   
+            validRace = true; 
+
         }else{System.out.println("Thats not a race, you silly goose! Try Again.");
             
         }
