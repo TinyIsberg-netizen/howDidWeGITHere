a282a63c97a508f2d30f60950c7dc683cabc7a64
Filip <isberget23@gmail.com>
2025-10-27 13:29:50 +0100

    Fleshed out character class

--- Recources/Character.java
+++ Recources/Character.java
@@ -1,17 +1,75 @@
 package Recources;
+
 abstract class Character {
 
-//Strength, Dexterity, Constitution
+    // Strength, Dexterity, Constitution
+    String name;
     int dexterity;
     int strength;
     int constitution;
-    
-public Character(int dexterity, int strength, int constitution) {
+    int attack = 10;
+    int maxHealth = 20;
+    int health = maxHealth;
+    int xp = 0;
+    int level = 1;
+
+    public Character(int dexterity, int strength, int constitution) {
         this.dexterity = dexterity;
         this.strength = strength;
         this.constitution = constitution;
-        
-    }
-    
-}
+        this.name = name;
 
+    }
+
+    public void attack(Enemy enemy) {
+        enemy.takeDamage(attack);
+        System.out.println(name + " attacked for " + attack);
+
+    }
+
+    public void takeDamage(int diceSize) {
+        health -= diceSize;
+        System.out.println(name + " takes" + diceSize);
+
+    }
+
+    public void gainXp(int amount) {
+        xp += amount;
+        System.out.println(name + " gain" + amount + " experience!");
+        if (xp >= level * 50) {
+            level++;
+            maxHealth += 10;
+            attack += 5;
+            health = maxHealth;
+            System.out.println("Level up! You are now " + level + "!");
+
+        }
+
+    }
+
+    public boolean isAlive() {
+        return health > 0;
+
+    }
+
+    public String getStatus() {
+        return name + " Health: " + "/" + maxHealth;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+
+    public String toString() {
+        return "\n ***Character Sheet***\n" +
+                "Name: " + name +
+                "\nLevel: " + level +
+                "\nHP: " + health + "/" + maxHealth +
+                "\nAttack: " + attack +
+                "\nXP: " + xp + "\n";
+    }
+
+}
