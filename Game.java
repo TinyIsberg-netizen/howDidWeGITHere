9167f5d06d8c6b3b52956e7c80b314a5e1b134ac
Filip <isberget23@gmail.com>
2025-10-27 17:24:07 +0100

    Started adding some stuff in game.java

---
+++ Game.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Game {
+    Scanner scan = new Scanner(System.in);
+    Player player;
+
+    public static void main(String[] args) {
+
+    }
+}
+
+public void start() {
+
+}
